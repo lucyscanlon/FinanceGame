78c3a579e197c31e836743dc15b1e0b0f6ce5467
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2023-12-16 14:11:50 +0000

    tested components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<HelloWorld msg="Hello There!"/> -->
+  <TestComponent testMessage="This is a test heading" testDescription="This is a test description"></TestComponent>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from './components/Test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestComponent,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="test-container">
+        <h1>{{testMessage}}</h1>
+        <p>{{testDescription}}</p>
+    </div>
+
+    
+  </template>
+  
+  <script>
+  export default {
+    name: 'TestComponent',
+    props: {
+      testMessage: String,
+      testDescription: String,
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style>
+ 
+
+  </style>
