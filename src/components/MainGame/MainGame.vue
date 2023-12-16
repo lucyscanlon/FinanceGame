b4e8494452aa0794170cba71d9914054dd35db7e
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2023-12-16 21:36:47 +0000

    added top and bottom banners displaying game information, added the map image display

--- src/components/MainGame/MainGame.vue
+++ src/components/MainGame/MainGame.vue
@@ -8,6 +8,23 @@ Elements that will be inside this container include:
 
 <template>
     <div class="maingame-wrapper">
+        <div className="gameplay-topbanner-wrapper">
+               <div className="current-date-wrapper">
+                        <p>Current Date: <span className="colour-white">01/01/2023</span></p>
+                    </div>
+                    <div className="payday-date-wrapper">
+                        <p>Payday: <span className="colour-white">The last day of the month</span></p>
+                    </div>
+            </div>
+        <MapDisplay></MapDisplay>
+        <div className="gameplay-topbanner-wrapper">
+               <div className="current-date-wrapper">
+                        <p>Current Date: <span className="colour-white">01/01/2023</span></p>
+                    </div>
+                    <div className="payday-date-wrapper">
+                        <p>Payday: <span className="colour-white">The last day of the month</span></p>
+                    </div>
+            </div>
 
     </div>
 
@@ -16,8 +33,13 @@ Elements that will be inside this container include:
 
 <script>
 
+import MapDisplay from './MapLayout.vue';
+
 export default {
     name: 'MainGameWrapper',
+    components: {
+        MapDisplay,
+    }
 }
 
 </script>
---
+++ src/components/MainGame/MapLayout.vue
@@ -0,0 +1,22 @@
+<!-- Template for the map layout
+
+Components that will be inside this container include:
+- living options information display components
+
+-->
+
+
+<template>
+    <div className="map-display-wrapper">
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MapDisplay',
+}
+
+</script>
