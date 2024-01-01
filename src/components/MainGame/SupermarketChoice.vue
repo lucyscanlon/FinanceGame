84fa776f1649aca059df57c86dba5123da47eded
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-01-01 17:35:18 +0000

    added the supermarket info content in SupermarketChoice.vue

--- src/components/MainGame/SupermarketChoice.vue
+++ src/components/MainGame/SupermarketChoice.vue
@@ -14,29 +14,53 @@ Components that will be inside this container include:
             <div class="supermarketchoice-box">
                 <div class="supermarketchoice-heading-wrap">
                     <h4>Saver Superstore</h4>
+                    <p>An affordable supermarket with budget and luxury products.</p>
+                </div>
+                <div class="supermarketchoice-costs-wrap">
+                    <p><span class="colour-pink">Cost:</span> £80pm</p>
+                    <p><span class="colour-pink">Transport Cost:</span> £+</p>
+                    
                 </div>
             </div>
              <div class="supermarketchoice-box">
                 <div class="supermarketchoice-heading-wrap">
                     <h4>Gourmet Grove</h4>
+                    <p>A luxury organic supermarket with high prices.</p>
+                </div>
+                <div class="supermarketchoice-costs-wrap">
+                    <p><span class="colour-pink">Cost:</span> £150pm</p>
+                    <p><span class="colour-pink">Transport Cost:</span> £+</p>
+                    
                 </div>
             
              </div>
              <div class="supermarketchoice-box">
                 <div class="supermarketchoice-heading-wrap">
                     <h4>Swiftbite Delivery</h4>
+                    <p>A food delivery app delivering from local restaurants.</p>
+                </div>
+                <div class="supermarketchoice-costs-wrap">
+                    <p><span class="colour-pink">Cost:</span> £300pm</p>
+                    <p><span class="colour-pink">Transport Cost:</span> £0</p>
+                    
                 </div>
             
             </div>
             <div class="supermarketchoice-box">
                 <div class="supermarketchoice-heading-wrap">
                     <h4>Weekly Mart Deliveries</h4>
+                    <p>A weekly grocery delivery service with budget prices.</p>
+                </div>
+                <div class="supermarketchoice-costs-wrap">
+                    <p><span class="colour-pink">Cost:</span> £100pm</p>
+                    <p><span class="colour-pink">Transport Cost:</span> £0</p>
+                    
                 </div>
             
             </div>
         </div>
         <div class="supermarketchoice-button-wrap">
-            <button>Confirm selection</button>
+            <button>Shop here</button>
         </div>
     </div>
     
