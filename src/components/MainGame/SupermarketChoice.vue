35ce28e101b57a1d3ca71021edc55af0d511f1ce
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-01-01 16:15:25 +0000

    created the template layout using flexbox and added supermarket titles for each option in SupermarketChoice.vue

--- src/components/MainGame/SupermarketChoice.vue
+++ src/components/MainGame/SupermarketChoice.vue
@@ -7,6 +7,38 @@ Components that will be inside this container include:
 
 
 <template>
+    <div class="supermarketchoice-display-wrapper">
+        <h1>Choose a supermarket or food service to purchase your groceries</h1>
+        <p class="supermarketchoice-description">Choose from one of the options below and press the 'confirm selection' button.</p>
+        <div class="supermarketchoice-flex-wrapper">
+            <div class="supermarketchoice-box">
+                <div class="supermarketchoice-heading-wrap">
+                    <h4>Saver Superstore</h4>
+                </div>
+            </div>
+             <div class="supermarketchoice-box">
+                <div class="supermarketchoice-heading-wrap">
+                    <h4>Gourmet Grove</h4>
+                </div>
+            
+             </div>
+             <div class="supermarketchoice-box">
+                <div class="supermarketchoice-heading-wrap">
+                    <h4>Swiftbite Delivery</h4>
+                </div>
+            
+            </div>
+            <div class="supermarketchoice-box">
+                <div class="supermarketchoice-heading-wrap">
+                    <h4>Weekly Mart Deliveries</h4>
+                </div>
+            
+            </div>
+        </div>
+        <div class="supermarketchoice-button-wrap">
+            <button>Confirm selection</button>
+        </div>
+    </div>
     
    
 </template>
