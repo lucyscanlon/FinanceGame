301f751dfb70ab77847e2217ed1d259b7bcb518a
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-01-02 18:59:08 +0000

    made a reuseable component for the transport options display in TransportOptionsFlexbox and added it to TransportOptionsDisplay.vue

--- src/components/MainGame/TransportOptionsDisplay.vue
+++ src/components/MainGame/TransportOptionsDisplay.vue
@@ -17,12 +17,8 @@ const livingOptions = useLivingOptionsStore()
         <h1>Choose a method of transport</h1>
         <p class="supermarketchoice-description">Choose from one of the options below and press the 'choose this method' button to confirm your choice</p>
         <div class="transportchoice-flexbox-wrap">
-            <div class="transport-choice-box">
-
-            </div>
-            <div class="transport-choice-box">
-                
-            </div>
+            <TransportChoicesFlex TransportName="Uber Driving Service" TransportDesc="A mobile app providing a private taxi service." TransportPerk="Private Driver" TransportCost="300" TransportCommuteCost="100" TransportGroceryCost="50" TransportDiscount="0"></TransportChoicesFlex>
+            <TransportChoicesFlex TransportName="Public Transport" TransportDesc="A well connected public transport service." TransportPerk="Under 25s get 33% off" TransportCost="150" TransportCommuteCost="40" TransportGroceryCost="15" TransportDiscount="33%"></TransportChoicesFlex>
         </div>
         <div @click="livingOptions.livingOptionsNextStageOfGame()" class="supermarketchoice-button-wrap">
                     <button>Shop here</button>
@@ -34,10 +30,12 @@ const livingOptions = useLivingOptionsStore()
 
 <script>
 
+import TransportChoicesFlex from './TransportOptionsFlexbox.vue';
 
 export default {
     name: 'TransportMethodDisplay', 
     components: {
+        TransportChoicesFlex,
     }
    
 }
---
+++ src/components/MainGame/TransportOptionsFlexbox.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <div class="transport-choice-box">
+                <div class="transport-name-desc-wrap">
+                    <h4>{{ TransportName }}</h4>
+                    <p>{{ TransportDesc }}</p>
+                </div>
+                <div class="transport-perk-wrap">
+                    <p><span class="colour-pink">Perk:</span> {{ TransportPerk }}</p>
+                </div>
+                <div class="transport-total-cost-wrap">
+                    <p>Costs:</p>
+                    <table>
+                        <tr>
+                            <th>Cost:</th>
+                            <th>+{{ TransportCost }}</th>
+                        </tr>
+                        <tr>
+                            <th>Commute Cost:</th>
+                            <th>+{{ TransportCommuteCost }}</th>
+                        </tr>
+                        <tr>
+                            <th>Grocery Transport:</th>
+                            <th>+{{ TransportGroceryCost }}</th>
+                        </tr>
+                        <tr>
+                            <th>Discount:</th>
+                            <th>-{{ TransportDiscount }}</th>
+                        </tr>
+                        <tr>
+                            <th class="colour-green">Total Cost:</th>
+                            <th class="colour-green">£450</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+</template>
+
+<script>
+
+
+
+export default {
+    name: 'TransportChoicesFlex', 
+    props: {
+        TransportName: String,
+        TransportDesc: String,
+        TransportPerk: String,
+        TransportCost: String,
+        TransportCommuteCost: String,
+        TransportGroceryCost: String,
+        TransportDiscount: String,
+        
+    }
+   
+   
+}
+
+</script>
