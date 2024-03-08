ca7b53acc3c281e17351f1d711392c0b644ebdf8
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-03-08 11:22:51 +0000

    added notifications panel in sidebar

--- src/components/CharacterSidebar/GameNotifications.vue
+++ src/components/CharacterSidebar/GameNotifications.vue
@@ -5,6 +5,9 @@ The parent of this component:
 
 -->
 <script setup>
+    import { useLivingOptionsStore } from '../../store/InitialGameChoicesStore'
+
+    const livingOptions = useLivingOptionsStore()
 
 </script>
 <template>
@@ -12,15 +15,18 @@ The parent of this component:
         <h4>Notifications</h4>
         <div class="characterstats-notifications-panel">
             <div class="notifications-padding">
-                <div class="game-notification-container notifications-current-focus">
-                    <p><span class="colour-pink">Current Goal:</span> Pay your bills on time!</p>
-                </div>
-                <div class="game-notification-container">
-                    <p>Your rent and bills are due</p>
-                </div>
+                        <div :class="(livingOptions.livingOptionsGameStage > 0) ? 'goal-notif-completed' : 'game-notification-container'" class=" notifications-current-focus">
+                            <p><span class="current-goal-notif">Current Goal:</span> Accept your job offer!</p>
+                        </div>
+                        <div class="game-notification-container">
+                            <p>Use the UK Tax calculator to see tax contributions for different incomes</p>
+                        </div>
+                        <div :class="(livingOptions.livingOptionsGameStage > 1) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                            <p>Your bills are due</p>
+                        </div>
             </div>
         </div>
-        <div class="pay-monthly-outgoings-container pay-monthly-inactive">
+        <div v-if="livingOptions.livingOptionsGameStage > 6" class="pay-monthly-outgoings-container pay-monthly-inactive">
                 <button>Pay monthly outgoings</button>
             </div>
         <hr>
