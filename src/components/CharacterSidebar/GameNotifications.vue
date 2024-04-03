10a3e3502e6c73bfe0515933e5294bca3a8a3370
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-04-03 03:10:53 +0100

    made animations of game notification panel more visually appealing

--- src/components/CharacterSidebar/GameNotifications.vue
+++ src/components/CharacterSidebar/GameNotifications.vue
@@ -5,25 +5,49 @@ The parent of this component:
 
 -->
 <script setup>
-    import { useLivingOptionsStore } from '../../store/InitialGameChoicesStore'
+import { useLivingOptionsStore } from '../../store/InitialGameChoicesStore'
+    import { useMainGameplayNavigationStore } from '../../store/MainGameChoicesStore.js'
 
     const livingOptions = useLivingOptionsStore()
+    const manageMainGameNav = useMainGameplayNavigationStore()
 
 </script>
 <template>
     <div class="characterstats-set">
         <h4>Notifications</h4>
         <div class="characterstats-notifications-panel">
-            <div class="notifications-padding">
-                        <div :class="(livingOptions.livingOptionsGameStage > 0) ? 'goal-notif-completed' : 'game-notification-container'" class=" notifications-current-focus">
-                            <p><span class="current-goal-notif">Current Goal:</span> Accept your job offer!</p>
+            <div v-if="manageMainGameNav.currentPage === 1" class="notifications-padding">
+                        <div :class="(manageMainGameNav.currentPage > 1) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                            <p>Accept your job offer!</p>
                         </div>
-                        <div class="game-notification-container">
+                        <div :class="(manageMainGameNav.currentPage > 1) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
                             <p>Use the UK Tax calculator to see tax contributions for different incomes</p>
                         </div>
-                        <div :class="(livingOptions.livingOptionsGameStage > 1) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                        <!--<div :class="(manageMainGameNav.currentPage > 1) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
                             <p>Your bills are due</p>
+                        </div>-->
+            </div>
+            <div v-if="manageMainGameNav.currentPage === 2" class="notifications-padding">
+                        <div :class="(manageMainGameNav.currentPage > 2) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                            <p>Set up a bank account</p>
                         </div>
+                        <!--<div :class="(manageMainGameNav.currentPage > 1) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                            <p>Your bills are due</p>
+                        </div>-->
+            </div>
+            <div v-if="manageMainGameNav.currentPage === 4" class="notifications-padding">
+                        <div :class="(manageMainGameNav.currentPage > 4) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                            <p>Choose a place to call home</p>
+                        </div>
+                        <div :class="(manageMainGameNav.currentPage > 4) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                            <p>Remember to stick to the 30-35% rule</p>
+                        </div>
+                        <div :class="(manageMainGameNav.currentPage > 4) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                            <p>35% of your income after tax is: £599.14</p>
+                        </div>
+                        <!--<div :class="(manageMainGameNav.currentPage > 1) ? 'goal-notif-completed' : 'game-notification-container'" class="game-notification-container">
+                            <p>Your bills are due</p>
+                        </div>-->
             </div>
         </div>
         <div v-if="livingOptions.livingOptionsGameStage > 6" class="pay-monthly-outgoings-container pay-monthly-inactive">
@@ -36,5 +60,17 @@ The parent of this component:
 // Export component data
 export default {
     name: 'GameNotificationPanel',
+    data() {
+        return {
+            currentPageVar: useMainGameplayNavigationStore().currentPage,
+        }
+    },
+    watch: {
+        currentPageVar(newValue, oldValue) {
+            if(newValue !== oldValue) {
+                console.log("watched")
+            }
+        }
+    }
 }
 </script>
--- src/store/MainGameChoicesStore.js
+++ src/store/MainGameChoicesStore.js
@@ -5,7 +5,7 @@ export const useMainGameplayNavigationStore = defineStore({
   id: "MainGameNavigationStore",
   state: () => ({
     mainGameComponentsUnlocked: 6,
-    currentPage: 12,
+    currentPage: 1,
   }),
   actions: {
     navigateToPage(num) {
