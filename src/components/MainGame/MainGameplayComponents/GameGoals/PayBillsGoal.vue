df8aaf6adf59fa8664b3864b44c1df5c42d3c74c
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-04-12 21:47:10 +0100

    fixed bug with the current goal component resetting when the second round of bills is paid

--- src/components/MainGame/MainGameplayComponents/GameGoals/PayBillsGoal.vue
+++ src/components/MainGame/MainGameplayComponents/GameGoals/PayBillsGoal.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="current-goal-colour-container">
-        <div :class="(manageGoals.currentGoal === manageGoals.completedGoals) ? 'goal-completed-styling' : ''" class="pension-interactive-title-container current-goal-title pay-bills-title">
+        <div :class="(manageGoals.completedGoals > 0) ? 'goal-completed-styling' : ''" class="pension-interactive-title-container current-goal-title pay-bills-title">
             <h1><font-awesome-icon icon="fa-solid fa-bullseye" />Current Goal</h1>
         </div>
         <div class="currentgoal-status-container">
             <p v-if="manageGoals.completedGoals === 0">Status: <span class="colour-red">Uncompleted</span></p>
             <p v-if="manageGoals.completedGoals > 0">Status: <span class="colour-green">Completed</span></p>
         </div>
-        <div :class="(manageGoals.currentGoal === manageGoals.completedGoals) ? 'currentgoal-completed-border' : ''" class="currentgoal-goal-container pay-bills-goal">
+        <div :class="(manageGoals.completedGoals > 0) ? 'currentgoal-completed-border' : ''" class="currentgoal-goal-container pay-bills-goal">
             <p><span class="colour-green">Goal:</span> Pay your bills on time!</p>
         </div>
         <div class="currentgoal-tips-container">
