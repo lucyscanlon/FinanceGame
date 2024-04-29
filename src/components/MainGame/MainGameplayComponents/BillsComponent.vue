e51162a4eb15d47aca9c22613931c7af84e1d99f
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-04-29 17:35:37 +0100

    added countdown in the bills component as well

--- src/components/MainGame/MainGameplayComponents/BillsComponent.vue
+++ src/components/MainGame/MainGameplayComponents/BillsComponent.vue
@@ -2,13 +2,19 @@
     <div class="maingameplay-flex-container">
         <div class="pension-interactive-title-container everydaysavings-title bills-interactive">
             <h1><font-awesome-icon icon="fa-solid fa-money-check-dollar" />Your Bills</h1>
-            <p>Pay your bills here. Bills are due at the end of each month.</p>
+            <p>Pay your bills here. Bills can be paid up to 10 days before the end of the month.</p>
         </div>
         <div class="billsinteractive-total-title-container">
             <p>Total Bills Amount:</p>
         </div>
         <div class="billsinteractive-total-container">
             <p>£{{ manageMoney.monthlyOutGoingsSum }}</p>
+        </div>
+        <div class="billsinteractive-total-title-container">
+            <p>Days until bills are due:</p>
+        </div>
+        <div class="billsinteractive-total-container">
+            <p>{{ manageGameTimer.countdown }}</p>
         </div>
         <!-- Buttons for the first goal - pay bills on time -->
         <div v-if="(manageGameTimer.countdown <= 5) && (manageMoney.billsPaid === manageGameTimer.monthsPassed) && (manageMoney.billsLate === false) && (manageGoals.currentGoal === 1)" class="pay-monthly-outgoings-container">
