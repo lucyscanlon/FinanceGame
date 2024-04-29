89dcfedc830d3db588159aae9868f0453866e75a
Lucy Scanlon <lucyscanlon@Lucys-MacBook-Pro.local>
2024-04-29 16:43:46 +0100

    added option for no income stream set up in sidebar

--- src/components/CharacterSidebar/IncomeStream.vue
+++ src/components/CharacterSidebar/IncomeStream.vue
@@ -19,25 +19,35 @@ The parent of this component:
     <div className="characterstats-set livingstatsdisplay">
         <h4>Extra Income Stream:</h4>
         <!--Use values from the store -->
-        <div class="incomestream-sidebar-type">
-            <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 1"><font-awesome-icon icon="fa-solid fa-shop" /> {{ manageIncomeStream.onlineStoreName }}</p>
-            <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 2"><font-awesome-icon icon="fa-solid fa-microphone" /> {{ manageIncomeStream.podcastName }}</p>
-            <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 3"><font-awesome-icon icon="fa-solid fa-martini-glass-citrus" /> {{ manageIncomeStream.barChoiceName }}</p>
-            <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 4"><font-awesome-icon icon="fa-solid fa-at" /> {{ manageIncomeStream.socialMediaUsername }}</p>
-            <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 5"><font-awesome-icon icon="fa-solid fa-computer" /> {{ manageIncomeStream.freelanceChosenSkill }}</p>
-        </div>
-        <div class="livingstats-display-rent-commute-cost-container">
-            <div class="livingstats-rent-display">
-                <p>Monthly Income:</p>
-                <p>Monthly Costs:</p>
+        <div v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier < 6" class="incomestream-choice-container">
+            <div class="incomestream-sidebar-type">
+                <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 1"><font-awesome-icon icon="fa-solid fa-shop" /> {{ manageIncomeStream.onlineStoreName }}</p>
+                <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 2"><font-awesome-icon icon="fa-solid fa-microphone" /> {{ manageIncomeStream.podcastName }}</p>
+                <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 3"><font-awesome-icon icon="fa-solid fa-martini-glass-citrus" /> {{ manageIncomeStream.barChoiceName }}</p>
+                <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 4"><font-awesome-icon icon="fa-solid fa-at" /> {{ manageIncomeStream.socialMediaUsername }}</p>
+                <p v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 5"><font-awesome-icon icon="fa-solid fa-computer" /> {{ manageIncomeStream.freelanceChosenSkill }}</p>
             </div>
-            <div class="livingstats-commute-display">
-                <p>£{{manageIncomeStream.chosenIncomeStreamChoice.ISmonthlyincome}}</p>
-                <p>£{{manageIncomeStream.chosenIncomeStreamChoice.ISmonthlycost}}</p>
+            <div class="livingstats-display-rent-commute-cost-container">
+                <div class="livingstats-rent-display">
+                    <p>Monthly Income:</p>
+                    <p>Monthly Costs:</p>
+                </div>
+                <div class="livingstats-commute-display">
+                    <p>£{{manageIncomeStream.chosenIncomeStreamChoice.ISmonthlyincome}}</p>
+                    <p>£{{manageIncomeStream.chosenIncomeStreamChoice.ISmonthlycost}}</p>
+                </div>
             </div>
+            <button @click="manageMainGameNav.navigateToPage(14), manageMoney.decreaseMonthlyOutGoings(Number(manageIncomeStream.chosenIncomeStreamChoice.ISmonthlycost))" className="moveout-button">Change</button>
+            <hr />
         </div>
-        <button @click="manageMainGameNav.navigateToPage(14), manageMoney.decreaseMonthlyOutGoings(Number(manageIncomeStream.chosenIncomeStreamChoice.ISmonthlycost))" className="moveout-button">Change</button>
-        <hr />
+        <div v-if="manageIncomeStream.chosenIncomeStreamChoice.ISidentifier === 6" class="incomestreams-nochoice-container">
+            <div class="incomestream-sidebar-type">
+                <p><font-awesome-icon icon="fa-solid fa-ban" /> No Income Stream</p>
+            </div>
+            <button @click="manageMainGameNav.navigateToPage(14), manageMoney.decreaseMonthlyOutGoings(Number(manageIncomeStream.chosenIncomeStreamChoice.ISmonthlycost))" className="moveout-button">Set one up</button>
+            <hr />
+            
+        </div>
     </div>
 </template>
 <script>
